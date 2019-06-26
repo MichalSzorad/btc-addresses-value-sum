efc0795d77b733fb6d84e76b5ba9cd624bf5d549
Michal Szorád <miso.szorad@gmail.com>
2019-06-26 19:02:47 +0200

    Add files via upload

---
+++ precache-manifest.5d38b04a1a4ba9e8daebb5a1d615826a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e8c02fe51c9801082fb4a007892a421a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "20e667b6d37bbe46855a",
+    "url": "/static/css/main.19393e92.chunk.css"
+  },
+  {
+    "revision": "20aeadd3aa22d1febb6f",
+    "url": "/static/js/2.1e18ffa8.chunk.js"
+  },
+  {
+    "revision": "20e667b6d37bbe46855a",
+    "url": "/static/js/main.56939ff0.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
