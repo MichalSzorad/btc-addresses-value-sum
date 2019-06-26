b7998bb69aab55a3d64adb0ed3685ff66c417d48
Michal Szorád <miso.szorad@gmail.com>
2019-06-26 19:04:00 +0200

    Remove some files

--- src/api.ts
+++
@@ -1,5 +0,0 @@
-export async function fetchBitcoinAddressValue(address: string): Promise<number> {
-  const response = await fetch('https://blockchain.info/q/addressbalance/' + address);
-  const text = await response.text();
-  return parseInt(text, 10)
-}
